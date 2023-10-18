712ef94a01243279f7e232c2eedc085d0ee24353
whatasoda <git@whatasoda.me>
2023-10-18 19:03:59 +0900

    chore: mock service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as ReactDOMClient from "react-dom/client";
 import { App } from "./App";
 import { worker } from "./mocks/worker";
-import "../public/mockServiceWorker.js?url";
 
 worker.start();
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import type { Plugin } from "vite";
+import path from "path";
 
 const mockServiceWorker = (): Plugin => ({
   name: "mock-service-worker",
   configureServer(server) {
     server.middlewares.use((req, _res, next) => {
       if (req.originalUrl === "/mockServiceWorker.js") {
-        req.url = `/@fs${process.cwd()}/public/mockServiceWorker.js`;
+        req.url = path.join(
+          "/@fs",
+          process.cwd(),
+          "public/mockServiceWorker.js"
+        );
       }
       next();
     });
